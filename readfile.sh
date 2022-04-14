9c20c95127920c7030edcd4f1da441af12709c57
Sanju <Sanju@gmail.com>
2022-04-14 13:59:41 +0000

    4th commit

--- readfile.sh
+++ readfile.sh
@@ -1,8 +1,8 @@
 #!/bin/bash 
 #Usage: Read and display contents of file in current folder
 
-ls > demo.txt
-for i in `cat demo.txt`
+#ls > demo.txt
+for i in `ls`
 do 
   echo "displaying contents of $i "
   cat $i
