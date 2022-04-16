55d0859295cd533e9bfc34098e0c0c8cc46f6aca
Ubuntu <ubuntu@ip-172-31-91-234.ec2.internal>
2022-04-16 13:37:32 +0000

    updated https_check

--- httpd_check.sh
+++ httpd_check.sh
@@ -1,4 +1,14 @@
 #!/bin/bash 
 #usage: check if httpdserver is installed and running 
 
+export status_var=`sudo systemctl status apache2 | grep inactive | wc -l`
 
+if [ "$status_var" == "0" ]; then 
+	echo " apache2 is installed on this system "
+
+elif [ "$status_var" == "1" ]; then 
+       echo " apache2 is NOT installed on this system" 
+       
+else 
+	echo " Not sure about apache2 . Please check it manually "
+fi 
