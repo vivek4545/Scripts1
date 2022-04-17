2cb3b0c38e72622215e7e640433d0fe42e3c06f6
vivek4545 <f16.vivek@gmail.com>
2022-04-17 14:34:23 +0530

    Update httpd_check.sh

--- httpd_check.sh
+++ httpd_check.sh
@@ -1,12 +1,12 @@
 #!/bin/bash 
 #usage: check if httpdserver is installed and running 
 
-export status_var=`sudo systemctl status apache2 | grep inactive | wc -l`
+export status_var=` systemctl status apache2 | grep 'Active: active (running)' | wc -l`
 
-if [ "$status_var" == "0" ]; then 
+if [ "$status_var" == "1" ]; then 
 	echo " apache2 is installed on this system "
 
-elif [ "$status_var" == "1" ]; then 
+elif [ "$status_var" == "0" ]; then 
        echo " apache2 is NOT installed on this system" 
        
 else 
