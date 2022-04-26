6c2dda48deba3e50c5e4dfc9112f967af52bf788
root <root@ip-172-31-17-193.ec2.internal>
2022-04-26 17:52:08 +0000

    new_1 commit

---
+++ install_tomcat.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+#This script is used to install tomcat manually.
+
+apt update -y
+apt install default-jdk
+wget https://dlcdn.apache.org/tomcat/tomcat-10/v10.0.20/bin/apache-tomcat-10.0.20.tar.gz
+tar -xvf apache-tomcat-10.0.20.tar.gz
+rm -rf apache-tomcat-10.0.20.tar.gz
+cd apache-tomcat-10.0.20/bin/
+./catalina.sh start
